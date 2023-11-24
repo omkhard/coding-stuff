24a0047b87ca1945c089e38eb69f63875974a378
omkhard <omkhard09@gmail.com>
2023-11-24 11:10:15 +0530

    epoch timestamp to Datetime conversion using time_t and localtime.

---
+++ epochToDatetime.cpp
@@ -0,0 +1,48 @@
+/*
+Q . Converting the epoch timestamp to datetime timestamp
+
+Earlier thought of creating a calendar function doing from scratch , it would be larger
+so optimised it using preimplimented functions and libraries .
+
+Author: om khard
+*/
+#include <bits/stdc++.h>
+using namespace std;
+// these function are unused and would be implimented on for a larger solution
+double secondsToMinutes(int sec){
+   return ((double)sec/60);
+}
+double minutesToHours(double min){
+  return (min/60);
+}
+double hoursToDays(double hrs){
+  return (hrs/24);
+}
+double calculateDaysFromEpoch(int epoch){
+  return hoursToDays(minutesToHours(secondsToMinutes(epoch)));
+}
+// Doing with Premimplimented library time.h
+struct tm* getDateTimeFromEpoch(time_t epoch){
+  struct tm* timeinfo;
+  timeinfo = localtime(&epoch);
+  return timeinfo;
+}
+
+string formatDateTime(struct tm* timeinfo){
+  char buf[80];
+  strftime(buf,sizeof(buf),"%Y-%m-%d %H:%M:%S",timeinfo);
+  string formatedDateTime = buf;
+  return formatedDateTime;
+}
+
+int main(){
+  int epoch = 1000; //seconds
+  // epoch starts from 1-Jan-1970
+  double days = calculateDaysFromEpoch(epoch);
+  std::cout<<days<<" days from epoch"<<endl;
+  time_t timefromEpoch = (time_t)epoch;
+  struct tm* timeinfo= getDateTimeFromEpoch(timefromEpoch);
+  string format = formatDateTime(timeinfo);
+  std::cout<<"Datetime("<<epoch<<"):"<<format;
+  return 1;
+}

fc14270b90c220a302740a49d17bbef6346f3055
omkhard <omkhard09@gmail.com>
2023-12-10 18:23:57 +0530

    Updated FIFO in elevator SysDesign.

--- elevatorSystemDesign.cpp
+++ elevatorSystemDesign.cpp
@@ -1,19 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
-//int NUM_OF_PEEPS=0; iski jaroorat ni h GLOBAL_VARIABLES are not much secure
+//int NUM_OF_PEEPS=0;
+/*
+This Elevator system is still a statically designed Elevator System
+which takes few use inputs and perform the opertations of Going 'UP/DOWN'
+preparing it like , first man leaves first and second leaves second
+
+problems:
+
+onces stoped , can't be started again
+*/
 class Elevator{
 private:
-  int totalPeeps,floor;
+  int totalPeeps,startAt,last_peep_floor;
   string elevatorDirection;
+  bool changeDirection=false;
 public:
   int stopAt;
-  Elevator(int startNumofPeeps,string direction,int floorElev,int stopat); // Constructor declared
+  int secondStart = startAt;
+  vector<int> secondStartAt;
+  Elevator(int startNumofPeeps,string direction,int startat,int stopat ,int lastPeepAt); // Constructor declared
   void inElevator(int num){
     totalPeeps = num+totalPeeps;
   }
   void outElevator(int num){
     if(num > totalPeeps){
-      std::cout<<"CAN'T EXIT !!! THEY ARENT THIS many in ELEVATOR"<<endl;
+      std::cout<<"CAN'T EXIT !!! THERE ARENT THIS many in ELEVATOR"<<endl;
       return;
     }
     totalPeeps = totalPeeps - num;
@@ -27,24 +39,28 @@ public:
   }
   void startElevator(){// this function will occur every time someone getsIn (pending...)
     while(1){
-      if(elevatorDirection=="up"){
-        std::cout<<"GOING UP::"<<endl;
-        floor = floor + 1;
-        sleep(2); // in 2 seconds it will reach other floor
 
+      if(elevatorDirection=="up" and changeDirection){
+        std::cout<<"GOING UP::"<<endl;
+        startAt = startAt + 1;
+        sleep(2); // in 2 seconds it will reach other floor
       }
-      if(elevatorDirection=="down"){
-        std::cout<<"GOING DOWN::"<<endl;
-        floor = floor - 1;
+      if(elevatorDirection=="down" and changeDirection){
+        std::cout<<"GOING DOWN::"<<startAt<<endl;
+        startAt = startAt - 1;
         sleep(2); // in 2 sec it reach other down floors
       }
-      if(floor == 5){
-        elevatorDirection = "down";
+      if(startAt == 6 or last_peep_floor < secondStart){
+        elevatorDirection = "down"; // it will go till 5th floor
+        changeDirection = true;
       }
-      if(floor == 0){
-        elevatorDirection = "up";
+      if(startAt == -1 or last_peep_floor > secondStart){
+        elevatorDirection = "up"; // it will go till 0(ground) floor
+        changeDirection = true;
+
       }
-      if(floor == stopAt){
+      if(startAt == last_peep_floor){
+        std::cout<<"LAST PEEP GOING OUT:";
         stopElevator();
         return ;
       }
@@ -58,16 +74,25 @@ public:
   }
 };
 // defined variables names should be different
-Elevator::Elevator(int startNumofPeeps,string direction,int floorElev,int stopat){
+Elevator::Elevator(int startNumofPeeps,string direction,int startat,int stopat,int lastPeepAt){
   totalPeeps = startNumofPeeps;
   elevatorDirection = direction;
-  floor = floorElev;
+  startAt = startat;
   stopAt = stopat;
+  last_peep_floor = lastPeepAt;
 }
 int main(){
-  int count=0;
+  int count=0,startAt,stop_at,init_peeps=2,last_peep_at;
   string dir;
-  Elevator elv(2,"up",2,3);
+  std::cout<<"Enter ElevAT:";
+  std::cin>>startAt;
+  std::cout<<"Enter StopAT:";
+  std::cin>>stop_at;
+  std::cout<<"Enter DirectionOfGoing(String):";
+  std::cin>>dir;
+  std::cout<<"Last peep leaving AT:";
+  std::cin>>last_peep_at;
+  Elevator elv(init_peeps,dir,startAt,stop_at,last_peep_at);
   elv.startElevator();
   count = elv.displayPeepsCount();
   dir = elv.detectDirection();

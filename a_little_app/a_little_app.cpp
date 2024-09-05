0e67b3db4d5ab17ac863a35889dd3f2748001819
omkhard <omkhard09@gmail.com>
2024-09-05 22:04:26 +0530

    little changes **git status

--- a_little_app/a_little_app.cpp
+++ a_little_app/a_little_app.cpp
@@ -1,19 +1,48 @@
 #include <iostream>
+#include <fstream>
 #include <string.h>
 
 using namespace std;
 
+/*
+Author:
+    Om Khard
+    
+Design: 
+    Making an application which :
+    1. Takes arguments from CLI as name and age and appends that to a
+    file like a db. 
+    (Write to a file named output.txt)
+    Using 'fstream' library 
+*/
+
+
+
 class DesignApplication{
     private:
-        int privA ,privB;
+        char* name;
+        int age;
     public:
-        DesignApplication(int a,int b):privA(a),privB(b){}
-        int performAddition(){
-            return privA+privB;
+    //Constructor
+        DesignApplication(char* a,int b):name(a),age(b){}
+    //Methods
+        char* getName(){
+            return name;
+        }
+        int getAge(){
+            return age;
+        }
+        void updateDB(){
+            ofstream myfile;
+        // this 'ios::app' parameter is used to append data.
+            myfile.open("./output.txt",std::ios::app);
+            myfile<<"Name: "<<name<<" Age:"<<age<<endl;
+            myfile.close();
         }
 };
 
 int main(int argc, char* argv[]){
-    DesignApplication d(atoi(argv[1]),atoi(argv[2]));
-    std::cout<<"HERES your pretty values "<<d.performAddition()<<endl;
+    DesignApplication d(argv[1],atoi(argv[2]));
+    std::cout<<"Name: "<<d.getName()<<endl<<"Age: "<<d.getAge();
+    d.updateDB();
 }
